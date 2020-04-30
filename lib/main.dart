413834bb0a24c1b3566ebd1b40629d8e4176d217
Keya Shah <mskeyashah@gmail.com>
2020-04-29 22:17:23 -0700

    feedback included

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,38 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:VeggieBuddie/loginPage.dart';
+import 'package:VeggieBuddie/onboardingscreen.dart';
 import 'package:camera/camera.dart';
 import 'package:VeggieBuddie/cameraPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+int initScreen;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  initScreen = await prefs.getInt("initScreen");
+  await prefs.setInt("initScreen", 1);
   try {
     cameras = await availableCameras();
   } on CameraException catch (e) {
     logError(e.code, e.description);
   }
-  runApp(LoginPage());
+  if(initScreen==0 || initScreen == null)
+    runApp(MyApp());
+  else
+    runApp(LoginPage());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: initScreen == 0 || initScreen == null ? "first" : "/",
+      routes: {
+        "first": (context) => OnboardingScreen(),
+      },
+    );
+  }
 }
---
+++ lib/onboardingscreen.dart
@@ -0,0 +1,231 @@
+import 'package:VeggieBuddie/loginPage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:VeggieBuddie/styles.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  @override
+  _OnboardingScreenState createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final int _numPages = 3;
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentPage = 0;
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> list = [];
+    for (int i = 0; i < _numPages; i++) {
+      list.add(i == _currentPage ? _indicator(true) : _indicator(false));
+    }
+    return list;
+  }
+
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 150),
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 8.0,
+      width: isActive ? 24.0 : 16.0,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.white : Color(0xFF7B51D3),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.1, 0.4, 0.7, 0.9],
+              colors: [
+                Color(0xFF3594DD),
+                Color(0xFF4563DB),
+                Color(0xFF5036D5),
+                Color(0xFF5B16D0),
+              ],
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 40.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: FlatButton(
+                    onPressed: () => runApp(LoginPage()),
+                    child: Text(
+                      'Skip',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Container(
+                  height: 530.0,
+                  child: PageView(
+                    physics: ClampingScrollPhysics(),
+                    controller: _pageController,
+                    onPageChanged: (int page) {
+                      setState(() {
+                        _currentPage = page;
+                      });
+                    },
+                    children: <Widget>[
+
+                      Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("Welcome to VeggieBuddie!",style: kTitleStyle,),
+                            SizedBox(height: 20.0),
+                            Center(
+                              child: Image(
+                                image: AssetImage(
+                                  'images/homepage.jpg',
+                                ),
+                                height: 270.0,
+                                width: 300.0,
+                              ),
+                            ),
+                            SizedBox(height: 28.0),
+                            Text(
+                              'Sign in with your google account when you have network to gain realtime access to the data.\n\nSign in as guest for offline use with the same functionalities.',
+                              style: kSubtitleStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Center(
+                              /*child: Image(
+                                image: AssetImage(
+                                  'assets/images/onboarding1.png',
+                                ),
+                                height: 300.0,
+                                width: 300.0,
+                              ),*/
+                            ),
+                            SizedBox(height: 30.0),
+                            Text(
+                              'Live your life smarter\nwith us!',
+                              style: kTitleStyle,
+                            ),
+                            SizedBox(height: 15.0),
+                            Text(
+                              'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+                              style: kSubtitleStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Personalize the allergens list and recieve an alert if the food contains the allergens.',
+                              style: kSubtitleStyle,
+                            ),
+                            SizedBox(height: 15.0),
+                            Center(
+                              child: Image(
+                                image: AssetImage(
+                                  'images/listpic.jpg',
+                                ),
+                                height: 340.0,
+                                width: 340.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: _buildPageIndicator(),
+                ),
+                _currentPage != _numPages - 1
+                    ? Expanded(
+                  child: Align(
+                    alignment: FractionalOffset.bottomRight,
+                    child: FlatButton(
+                      onPressed: () {
+                        _pageController.nextPage(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.ease,
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            'Next',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                          SizedBox(width: 10.0),
+                          Icon(
+                            Icons.arrow_forward,
+                            color: Colors.white,
+                            size: 30.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+                    : Text(''),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomSheet: _currentPage == _numPages - 1
+          ? Container(
+        height: 80.0,
+        width: double.infinity,
+        color: Colors.white,
+        child: GestureDetector(
+          onTap: () => runApp(LoginPage()),
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 30.0),
+              child: Text(
+                'Get started',
+                style: TextStyle(
+                  color: Color(0xFF5B16D0),
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
+          : Text(''),
+    );
+  }
+}
